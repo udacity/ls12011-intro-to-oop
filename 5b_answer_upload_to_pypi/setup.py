ebd57aa89748d485d084c48a2b634b823b5665fb
Kyungwon Chun <ruddyscent@gmail.com>
2025-02-17 02:46:11 +0900

    Add answer to upload_to_pypi.
    - Renamed `5_exercise_upload_to_pypi` to `5a_upload_to_pypi`
    - Added `5b_answer_upload_to_pypi/dsnd_probability` package with probability distributions
    - Updated `upload_to_pypi.txt` to reflect new folder structure
    - Included `setup.py`, `setup.cfg`, and `LICENSE.txt` for packaging

---
+++ 5b_answer_upload_to_pypi/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dsnd_probability",
+    version="1.2",
+    description="Gaussian and Binomial distributions",
+    packages=["dsnd_probability"],
+    author="Andrew Paster",
+    author_email="myemail@something.com",
+    zip_safe=False,
+)
